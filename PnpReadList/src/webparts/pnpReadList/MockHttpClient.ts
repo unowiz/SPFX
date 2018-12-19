cfea54da947c9976015d04c72abd4c5c87112da4
Americo <perezamerico451@gmail.com>
2018-12-19 17:35:22 +0100

    pnp

---
+++ PnpReadList/src/webparts/pnpReadList/MockHttpClient.ts
@@ -0,0 +1,16 @@
+import { ISPList } from "./PnpReadListWebPart";
+
+export default class MockHttpClient {
+    private static _items: ISPList[] = [
+        { Title: 'Barclays'},
+        { Title: 'Press Office in Melborne'},
+        { Title: 'Vodaphone'},
+        { Title: 'Manager in New York'},
+    ];
+
+    public static get(): Promise<ISPList[]> {
+        return new Promise<ISPList[]>((resolve) => {
+            resolve(MockHttpClient._items);
+        });
+    }
+} 

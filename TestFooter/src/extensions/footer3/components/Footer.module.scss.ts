ef85ee05ef6e38a0d75185c7742d768e1739fd05
Americo <americo.perez@icloud.com>
2019-01-30 16:46:29 +0100

    deleted

--- TestFooter/src/extensions/footer3/components/Footer.module.scss.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-require("./Footer.module.css");
-const styles = {
-  footer: 'footer_58738bd0',
-  main: 'main_58738bd0',
-};
-
-export default styles;
-/* tslint:enable */
--- TestFooter/src/extensions/footer3/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface IFooter3ApplicationCustomizerStrings {
-  Title: string;
-}
-
-declare module 'Footer3ApplicationCustomizerStrings' {
-  const strings: IFooter3ApplicationCustomizerStrings;
-  export = strings;
-}

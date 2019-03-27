0bb68acae72e8ac136bff8d4e6b65494df7a7d45
jarkaos <americo.perez@icloud.com>
2019-03-28 00:13:04 +0100

    removed agreement

--- Agreement/src/container/AgreementContainer.module.scss.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-require('./AgreementContainer.module.css');
-const styles = {
-  agreementDatabase: 'agreementDatabase_46039ab5',
-};
-
-export default styles;
-/* tslint:enable */

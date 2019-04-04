5391c6b84d5761a9d253cf0d549c80d68ac6b507
Americo <americo.perez@icloud.com>
2019-04-04 08:31:43 +0200

    Lastet

---
+++ AgreementDatabase/lib/webparts/agreementDatabase/components/AgreementDatabase.module.scss.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+    'main-box': string;
+    link: string;
+    'adw-link-box-container': string;
+    'link-box': string;
+    'link-box-header-container': string;
+    'link-box-header-title': string;
+    'link-box-header-text': string;
+    separator: string;
+};
+export default styles;
